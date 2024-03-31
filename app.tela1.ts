26d16aa9796cabe4d9cd863f562b22728b5571f0
Italosant21 <147346126+Italosant21@users.noreply.github.com>
2024-03-31 17:48:34 -0300

    Create app.tela1.ts

---
+++ app.tela1.ts
@@ -0,0 +1,14 @@
+// app.component.ts
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {profilePic: sring = 'assets/img-profile-pic.jpg';
+
+  Picture() {
+  
+  }
+}
